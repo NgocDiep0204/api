5166da817abb63caabfbdc9475773ffad5c0745b
NgocDiep0204 <htndiep2277@gmail.com>
2024-12-06 23:46:22 +0700

    ge

---
+++ api/DTOs/UserUP.cs
@@ -0,0 +1,9 @@
+﻿namespace api.DTOs;
+
+public class UserUP
+{
+    public string? UserName { get; set; }
+    public string? FullName { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+}
